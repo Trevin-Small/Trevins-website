6cbcb02b0900d50dcbeb135a0b18bf06d75e3295
Trevin Small <trevincub03@gmail.com>
2021-11-17 16:14:01 -0500

    Delete scale_site.js

--- scale_site.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    window.onresize = function() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-    }; 
-}
