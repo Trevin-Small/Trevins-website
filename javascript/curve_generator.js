452b00a7402c050a87d22f07ea78830ebb314293
Trevin Small <trevincub03@gmail.com>
2022-01-14 18:03:29 -0500

    For the first time in forever
    light theme looked better ngl

--- javascript/curve_generator.js
+++ javascript/curve_generator.js
@@ -253,7 +253,7 @@ var colorSchemes = [
     // Background, Table, Side-bar, Highlight
     ['#160025','#4f0147' ,'#4f0147'], // Dark: Purples
     ['#041C32', '#064663', '#ECB365'], // Dark: blue-green and yellow
-    ['#041C32', '#17c3b2', '#227c9d'] // Light: Greens
+    ['#fef9e', '#17c3b2', '#227c9d'] // Light: Greens
 ]
 
 window.onload = function() {
